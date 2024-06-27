657a91b62b1e2af503a1d1981ded6e242d0ee681
sangwook16074 <qazws78941@gmail.com>
2024-06-27 20:31:14 +0900

    프로젝트 생성

---
+++ src/main/kotlin/com/example/checkcheck/CheckcheckApplication.kt
@@ -0,0 +1,11 @@
+package com.example.checkcheck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CheckcheckApplication
+
+fun main(args: Array<String>) {
+	runApplication<CheckcheckApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/checkcheck/CheckcheckApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.checkcheck
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CheckcheckApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
