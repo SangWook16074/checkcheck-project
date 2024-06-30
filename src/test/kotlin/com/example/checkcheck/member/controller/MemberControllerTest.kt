34dc654fd268a0da506cb47026d5ce800f9fce91
sangwook16074 <qazws78941@gmail.com>
2024-07-01 00:30:04 +0900

    컨트롤러 테스트 코드 추가

---
+++ src/test/kotlin/com/example/checkcheck/member/controller/MemberControllerTest.kt
@@ -0,0 +1,162 @@
+package com.example.checkcheck.member.controller
+
+import com.example.checkcheck.member.dto.SignUpDto
+import com.example.checkcheck.member.service.MemberService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest(MemberController::class)
+@AutoConfigureMockMvc
+class MemberControllerTest(
+
+) {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockkBean
+    private lateinit var memberService: MemberService
+
+    val testEmail = "test@test.com"
+    val testPassword = "testtest1@"
+    val testName = "test"
+    val signUpDto = SignUpDto(
+        testEmail, testPassword, testName
+    )
+
+    @Test
+    fun `회원가입시 클라이언트는 201의 응답코드를 전달받는다`() {
+        every { memberService.signup(signUpDto) } returns "회원가입이 완료되었습니다!"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/member/signup")
+                .content("{\"email\":\"test@test.com\", \"password\":\"testtest1@\", \"name\":\"test\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isCreated
+        )
+    }
+
+    @Test
+    fun `회원가입시 이메일 형식을 지키지 않는 DTO는 에러가 반환된다`() {
+        every { memberService.signup(signUpDto) } returns "회원가입이 완료되었습니다!"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/member/signup")
+                .content("{\"email\":\"testtest.com\", \"password\":\"testtest1@\", \"name\":\"test\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isBadRequest
+        ).andExpect(
+            content().contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            jsonPath("$.status").value("ERROR")
+        )
+            .andExpect(
+                jsonPath("$.data._email").value("올바르지 못한 이메일 형식입니다!")
+            )
+            .andExpect(
+                jsonPath("$.resultMsg").value("에러가 발생했습니다!")
+            )
+    }
+
+    @Test
+    fun `회원가입시 비밀번호가 8자이상 20자 이하 영소대소문자특수문자를 포함하는 형식을 지키지 않는 DTO는 에러가 반환된다`() {
+        every { memberService.signup(signUpDto) } returns "회원가입이 완료되었습니다!"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/member/signup")
+                .content("{\"email\":\"testtest.com\", \"password\":\"testtes\", \"name\":\"test\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isBadRequest
+        ).andExpect(
+            content().contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            jsonPath("$.status").value("ERROR")
+        )
+            .andExpect(
+                jsonPath("$.data._password").value("올바르지 못한 비밀번호 형식입니다!")
+            )
+            .andExpect(
+                jsonPath("$.resultMsg").value("에러가 발생했습니다!")
+            )
+    }
+
+    @Test
+    fun `회원가입시 이메일을 입력하지 않는 DTO는 에러가 반환된다`() {
+        every { memberService.signup(signUpDto) } returns "회원가입이 완료되었습니다!"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/member/signup")
+                .content("{\"password\":\"testtest1@\", \"name\":\"test\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isBadRequest
+        ).andExpect(
+            content().contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            jsonPath("$.status").value("ERROR")
+        )
+            .andExpect(
+                jsonPath("$.data._email").value("이메일을 입력해주세요!")
+            )
+            .andExpect(
+                jsonPath("$.resultMsg").value("에러가 발생했습니다!")
+            )
+    }
+
+    @Test
+    fun `회원가입시 비밀번호를 입력하지 않는 DTO는 에러가 반환된다`() {
+        every { memberService.signup(signUpDto) } returns "회원가입이 완료되었습니다!"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/member/signup")
+                .content("{\"email\":\"testtest.com\", \"name\":\"test\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isBadRequest
+        ).andExpect(
+            content().contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            jsonPath("$.status").value("ERROR")
+        )
+            .andExpect(
+                jsonPath("$.data._password").value("비밀번호를 입력해주세요!")
+            )
+            .andExpect(
+                jsonPath("$.resultMsg").value("에러가 발생했습니다!")
+            )
+    }
+
+    @Test
+    fun `회원가입시 이름을 입력하지 않는 DTO는 에러가 반환된다`() {
+        every { memberService.signup(signUpDto) } returns "회원가입이 완료되었습니다!"
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/member/signup")
+                .content("{\"email\":\"testtest.com\", \"password\":\"testtest1@\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            status().isBadRequest
+        ).andExpect(
+            content().contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+            jsonPath("$.status").value("ERROR")
+        )
+            .andExpect(
+                jsonPath("$.data._name").value("이름을 입력해주세요!")
+            )
+            .andExpect(
+                jsonPath("$.resultMsg").value("에러가 발생했습니다!")
+            )
+    }
+}
