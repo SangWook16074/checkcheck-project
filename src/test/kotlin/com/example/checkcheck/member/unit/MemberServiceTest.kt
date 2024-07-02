4a40e9f0535d82b75a8bd641742e7f4e6dbad854
yoonhyojin <dbsgywls9855@gmail.com>
2024-07-02 17:49:12 +0900

    테스트 코드에서 security config 제외

--- src/test/kotlin/com/example/checkcheck/member/unit/MemberServiceTest.kt
+++ src/test/kotlin/com/example/checkcheck/member/unit/MemberServiceTest.kt
@@ -1,7 +1,9 @@
 package com.example.checkcheck.member.unit
 import com.example.checkcheck.common.authority.JwtTokenProvider
+import com.example.checkcheck.common.enums.Role
 import com.example.checkcheck.member.dto.SignUpDto
 import com.example.checkcheck.member.entity.Member
+import com.example.checkcheck.member.entity.MemberRole
 import com.example.checkcheck.member.repository.MemberRepository
 import com.example.checkcheck.member.repository.MemberRoleRepository
 import com.example.checkcheck.member.service.MemberService
@@ -32,15 +34,24 @@ class MemberServiceTest {
         name = testName
     )
 
+    val memberRole = MemberRole(
+        id = null,
+        role = Role.MEMBER,
+        member = member
+    )
+
+
     @Test
     fun `회원가입에 성공할 경우 성공 메시지가 반환된다`() {
+        every { memberRoleRepository.save(any()) } returns memberRole
         every { memberRepository.save(any()) } returns member
         every { memberRepository.findByEmail(testEmail) } returns null
 
         val result = memberService.signup(signUpDto)
 
-        verify(exactly = 1) { memberRepository.save(any()) }
+        verify(exactly = 2) { memberRepository.save(any()) }
         verify(exactly = 1) { memberRepository.findByEmail(signUpDto.email) }
+        verify(exactly = 1) { memberRoleRepository.save(any()) }
 
         assertEquals("회원가입이 완료되었습니다!", result)
 
