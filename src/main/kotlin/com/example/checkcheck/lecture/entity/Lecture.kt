0e2a276fa391a0a3bcbd91c5860a121e670bef25
wjddnalt24@gmail.com <wjddnalt24@gmail.com>
2024-07-03 16:17:24 +0900

    상욱이 코드 합칠꺼임

--- src/main/kotlin/com/example/checkcheck/lecture/entity/Lecture.kt
+++ src/main/kotlin/com/example/checkcheck/lecture/entity/Lecture.kt
@@ -1,11 +1,7 @@
 package com.example.checkcheck.lecture.entity
 
 import com.example.checkcheck.lecture.dto.LectureResponseDto
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
@@ -33,6 +29,7 @@ class Lecture(
     //강의정보
     @Column(nullable = false, updatable = false, length = 200)
     var information: String
+
 ) {
     fun toResponse() : LectureResponseDto = LectureResponseDto(
         title = title,
