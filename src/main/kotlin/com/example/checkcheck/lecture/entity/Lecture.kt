5522a250633bc2b865f24e4e4a60963bf31ada41
wjddnalt24@gmail.com <wjddnalt24@gmail.com>
2024-07-09 13:24:53 +0900

    강의 취소 및 시간변경, 개별강의조회 추가

--- src/main/kotlin/com/example/checkcheck/lecture/entity/Lecture.kt
+++ src/main/kotlin/com/example/checkcheck/lecture/entity/Lecture.kt
@@ -1,13 +1,8 @@
 package com.example.checkcheck.lecture.entity
 
 import com.example.checkcheck.lecture.dto.LectureResponseDto
-<<<<<<< HEAD
 import jakarta.persistence.*
-import java.time.LocalDateTime
-=======
 import com.example.checkcheck.member.entity.Member
-import jakarta.persistence.*
->>>>>>> feature/lecture
 
 @Entity
 @Table(
@@ -24,24 +19,9 @@ class Lecture(
     @Column(nullable = false, length = 100)
     var maxStudent : Int,
 
-<<<<<<< HEAD
-    //강의시간
-    @Column(nullable = false, updatable = false, length = 50)
-    var time: String,
-
-    //수강신청기간
-    @Column(nullable = false, updatable = false, length = 50)
-    var period: String,
-
-    //강의정보
-    @Column(nullable = false, updatable = false, length = 200)
-    var information: String
-
-=======
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(foreignKey = ForeignKey(name = "fk_lecture_id_member_id"))
     var member: Member
->>>>>>> feature/lecture
 ) {
 
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "lecture", cascade = [CascadeType.ALL])
